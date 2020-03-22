a0019d48175ad2b60f7e6980bea1b64e5d6f7e6b
Yago Quiñoy Lobariñas <yago.quinoy@cabify.com>
2020-03-22 15:22:45 +0100

    Add cra typescript auto-config

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const sum = (a:number, b:number): number => {
+    return a + b;
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{`Sum result 3 + 4 = ${ sum(3, 4) }`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
